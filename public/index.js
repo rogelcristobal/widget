1059017add22e44429244119a11aed8d563456dc
rogelcristobal <rogel.cristobal11@gmail.com>
2022-07-18 14:39:05 +0800

    added a js file

--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
-const inputTitle = document.querySelector('#title-input')
+{
+    let toggleTitle = false
+    const inputTitle = document.querySelector('#title-input')
+    const titleBtn = document.querySelector('#title-btn').addEventListener('click',()=>{
+        if(!toggleTitle){
+            inputTitle.style.display = "none"  
+            toggleTitle = true
+        }else{
+            inputTitle.style.display = 'grid'    
+            toggleTitle = false
+        }
+            
+        
+    })
+}

5fc6089ee3d2d70a4c28e38422ff99104fefe8cc
rogel cristobal <rogel.cristobal11@gmail.com>
2022-07-18 19:52:54 +0800

    added bar mpn

--- public/index.js
+++ public/index.js
@@ -1,40 +1,48 @@
 {
 	let toggleTitle = false;
 	let toggleInBox = false;
-	const inputTitle = document.querySelector("#title-input");
-	const inBoxInput = document.querySelector("#inBox-input");
-	const inBoxBtn = document.querySelector("#inBox-btn");
+
+	const titleContainer = document.querySelector("#title-container");
+	const mpnContainer = document.querySelector("#mpn-container");
+	// btns
+	const mpnBtn = document.querySelector("#inBox-btn");
 	const titleBtn = document.querySelector("#title-btn");
 	const clr = document.querySelector("#clear-btn");
 
+	// input field
+	const inputTitle = document.querySelector("#title-input");
+	const mpn = document.querySelector("#mpn-input");
+
+	// functions
 	titleBtn.addEventListener("click", function () {
 		if (!toggleTitle) {
-			inputTitle.style.display = "grid";
+			titleContainer.style.display = "grid";
 			titleBtn.style.borderColor = "black";
 			titleBtn.style.color = "black";
 			toggleTitle = true;
 		} else {
-			inputTitle.style.display = "none";
+			titleContainer.style.display = "none";
 			titleBtn.style.borderColor = " rgb(163, 163, 163)";
 			titleBtn.style.color = " rgb(163, 163, 163";
 			toggleTitle = false;
 		}
 	});
 
-	inBoxBtn.addEventListener("click", function () {
+	mpnBtn.addEventListener("click", function () {
 		if (!toggleInBox) {
-			inBoxInput.style.display = "grid";
-			inBoxBtn.style.borderColor = "black";
-			inBoxBtn.style.color = "black";
+			mpnContainer.style.display = "grid";
+			mpnBtn.style.borderColor = "black";
+			mpnBtn.style.color = "black";
 			toggleInBox = true;
 		} else {
-			inBoxInput.style.display = "none";
-			inBoxBtn.style.borderColor = " rgb(163, 163, 163)";
-			inBoxBtn.style.color = " rgb(163, 163, 163";
+			mpnContainer.style.display = "none";
+			mpnBtn.style.borderColor = " rgb(163, 163, 163)";
+			mpnBtn.style.color = " rgb(163, 163, 163";
 			toggleInBox = false;
 		}
 	});
 	clr.addEventListener("click", function () {
-		alert(inputTitle.value);
+		inputTitle.value = "";
+		mpn.value = "";
 	});
 }

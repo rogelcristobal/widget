535f05d168a097a974ed01a10d440d0639e026e1
rogelcristobal <rogel.cristobal11@gmail.com>
2022-07-18 13:22:04 +0800

    my commit

---
+++ public/index.js
@@ -0,0 +1,1 @@
+const inputTitle = document.querySelector('#title-input')
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        "outfit":["Outfit","sans-serif"],
+        "satoshi":["Satoshi","sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
